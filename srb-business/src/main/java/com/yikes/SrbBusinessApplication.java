f73c807196b392fb8a360cbc1b97f9382758b69e
guanrong.yin <yikes_ygr@163.com>
2023-09-30 23:35:04 +0800

    搭建框架1

---
+++ srb-business/src/main/java/com/yikes/SrbBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.yikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrbBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrbBusinessApplication.class, args);
+    }
+
+}
